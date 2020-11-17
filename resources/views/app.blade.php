2ab44ad09a3df6ce61cf9ce2b41a6fbf61fdc66a
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-11-17 15:44:25 +0100

    initialize project, introduce vehicles model, make first migration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Incidenta</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+        </nav>
+    </div>
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/vehicles.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+    <h1>Fahrzeuge</h1>
+    {{ $vehicles }}
+</div>
+@endsection
