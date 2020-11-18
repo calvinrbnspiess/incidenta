3f61dc6f00ac1fa97e476bf4c8c9bdea12fe7fe2
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-11-18 13:40:49 +0100

    install laravel-ide-helper, generate php docs for models

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,7 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Vehicle
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $radioIdentificationPrefix
+ * @property string $radioIdentification
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereRadioIdentification($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereRadioIdentificationPrefix($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vehicle whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Vehicle extends Model
 {
+
+    protected $fillable = ['name', 'radioIdentificationPrefix', 'radioIdentification'];
+
     use HasFactory;
 }
