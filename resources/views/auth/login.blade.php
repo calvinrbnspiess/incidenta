4b1be7e5cc3d5f5ae1427c7af96b500ac95e68d2
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-11-22 11:50:26 +0100

    change login page strings

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,12 +35,12 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Passwort vergessen?') }}
                     </a>
                 @endif
 
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 pl-2" href="{{ route('register') }}">
-                    {{ __('Register') }}
+                    {{ __('Registrieren') }}
                 </a>
 
                 <x-jet-button class="ml-4">
